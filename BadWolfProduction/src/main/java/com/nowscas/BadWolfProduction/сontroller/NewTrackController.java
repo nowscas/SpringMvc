49c538ea1fd3ea17a3e07b205c23de364e127bb1
nowscas <wowyalta@inbox.ru>
2019-01-10 11:58:48 +0300

    перенес добавление нового трека в отдельный контроллер

--- BadWolfProduction/src/main/java/com/nowscas/BadWolfProduction/сontroller/NewTrackController.java
+++ BadWolfProduction/src/main/java/com/nowscas/BadWolfProduction/сontroller/NewTrackController.java
@@ -11,33 +11,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @Controller
-public class MainController {
+public class NewTrackController {
     @Autowired
     private AudioTrackRepo audioTrackRepo;
 
-    @GetMapping("/")
-    public String greeting(Map<String, Object> model) {
-        return "greeting";
+    @GetMapping("/addNewTrack")
+    public String getNewTrackTemplate(){
+        return "addNewTrack";
     }
 
-    @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    @PostMapping("/addNewTrack")
+    public String addTrack(@RequestParam String description, @RequestParam String singer, Map<String, Object> model){
+        AudioTrack audioTrack = new AudioTrack(description, singer);
+
+        audioTrackRepo.save(audioTrack);
+
         Iterable<AudioTrack> tracks = audioTrackRepo.findAll();
         model.put("tracks", tracks);
-        return "main";
+        return "redirect:/main";
     }
 
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model){
-        Iterable<AudioTrack> audioTracks;
-
-        if (filter != null && !filter.isEmpty()) {
-            audioTracks = audioTrackRepo.findByTrackSinger(filter);
-        }
-        else {
-            audioTracks = audioTrackRepo.findAll();
-        }
-        model.put("tracks", audioTracks);
-        return "main";
-    }
 }
