d27f0bb1081b332dd617185d4cb528a5d4564552
nowscas <wowyalta@inbox.ru>
2019-01-13 15:50:30 +0300

    Установил неограниченное время подключения к сервлету и максимальный размер загружаемого файла (50). Добавил обработку аудио и видеофайлов при загрузке картинок для аватарок.

--- BadWolfProduction/src/main/java/com/nowscas/BadWolfProduction/Application.java
+++ BadWolfProduction/src/main/java/com/nowscas/BadWolfProduction/Application.java
@@ -1,12 +1,40 @@
 package com.nowscas.BadWolfProduction;
 
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("50MB");
+        factory.setMaxRequestSize("50MB");
+        return factory.createMultipartConfig();
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory tomcatEmbedded(){
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+                //-1 means unlimited
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        });
+        return tomcat;
     }
 }
